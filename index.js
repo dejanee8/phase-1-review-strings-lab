449be28131640e4721b206d0de4ffc9bb300af0a
Dejanee Gomez <dejaneegomez@gmail.com>
2024-01-08 21:42:28 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Dejanee';
+const welcomeMessage = 'Welcome to Flatbook,  ${Dejanee}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${D.slice(0, 1)}!';
+
+
